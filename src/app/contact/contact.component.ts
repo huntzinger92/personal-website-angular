79d0c5b646f0a0d35520762df99ac7d02428062e
Trevor T. Smith <tts2p4@gmail.com>
2020-01-14 10:56:51 -0600

    accepting an error status of 200 as message successfully sent, otherwise alert user that message was not successfully sent

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -30,12 +30,20 @@ export class ContactComponent implements OnInit {
       res => {},
       err => {
         if (err instanceof ErrorEvent) {
+          //client side error
           alert("Something went wrong when sending your message.");
           console.log(err.error.message);
         } else {
-          alert("Your message has been sent!");
-          console.log(err.status);
-          console.log(err.error);
+          //backend error. If status is 200, then the message successfully sent
+          if (err.status === '200') {
+            alert("Your message has been sent!");
+          } else {
+            alert("Something went wrong when sending your message.");
+            console.log('Error status:');
+            console.log(err.status);
+            console.log('Error body:');
+            console.log(err.error);
+          };
         };
       }
     );
