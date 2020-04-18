00e9c3979c9815270fe840250aaf4b3211339ca4
Trevor T. Smith <tts2p4@gmail.com>
2020-04-17 19:38:44 -0500

    Lots of stuff (sorry). Paypal integration on Music tab. Prompts user for donation on any link, will not prompt after one donation (will prompt on subsequent visits). Also added 2-5-1 voicings entry in music tab, as well as meta tags for Facebook (in the index.html file).

--- src/app/music.ts
+++ src/app/music.ts
@@ -1,19 +1,40 @@
 export const musical_works = [{
-  id: 'solo-transcriptions',
+  id: '251-voicings',
+  title: "Favorite Two Five One Voicings (2020)",
+  imgSrc: 'assets\\photos\\251-voicings.png',
+  imgAlt: "Sample of voicings",
+  imgId: 'voicings-sample',
+  has2ndImg: false,
+  notesId: '251-notes',
+  description: [
+    `One of my goals during the COVID-19 epidemic of 2020 (suddenly having more practice time on my hands) was to come out of it having developed a richer harmonic vocabulary
+    for comping over two five one chord progressions, especially using polychords and quartal harmony in the style of my favorite jazz pianists, like McCoy Tyner and Benny Green.
+    To make this easier, I compiled a list my favorite 2-5-1 voicings into a single pdf.`,
+    `I'm sharing it here as an educational resource - one that I hope makes it easier for
+    other jazz players and composers to find ways to get away from the typical triadic voicings in their comping and arranging.`
+  ],
+  links: [
+    {href: 'assets\\scores\\251-voicings-final.pdf', text: "Sheet Music"},
+  ]
+},
+{
+  id: 'morgans-song',
   title: "Morgan's Song for Wind Ensemble (2020)",
   imgSrc: 'assets\\photos\\morgans-song-score.png',
   imgAlt: "Morgan's song score",
   imgId: 'morgans-song-score',
   has2ndImg: false,
   notesId: 'morgans-song-notes',
-  description: `<p class='entry-notes' id='test'>"Morgan’s Song" was originally composed as a chamber work by my grandfather, Dr. Charles W. Smith. It is dedicated to his
+  description: [
+    `"Morgan’s Song" was originally composed as a chamber work by my grandfather, Dr. Charles W. Smith. It is dedicated to his
   granddaughter, my sister, Morgan Smith, whose birth coincided closely with the music’s genesis. The composition has become one of his most performed pieces, and
   it seems that the work of arranging it for new ensembles has fallen to me, now that my grandfather has passed away. Dr. Janet Bass Smith (my grandmother - quite
   the family affair) commissioned this arrangement for wind band, and I have added my own voice to the original work. The music begins in my grandfather’s consonant,
-  pastoral manner but, as the piece progresses, becomes increasingly dissonant and complex in a style more akin to my own.</p>
-  <p>For me, this gradual change is a kind of musical metaphor for how the baton of one musical generation gets passed on to the next. Although we are unique
+  pastoral manner but, as the piece progresses, becomes increasingly dissonant and complex in a style more akin to my own.`
+  ,
+  `For me, this gradual change is a kind of musical metaphor for how the baton of one musical generation gets passed on to the next. Although we are unique
   individuals, we are indelibly shaped by who came before us, not just as artists, but in totality as human beings. I will always be grateful for the influence
-  and paths made possible for me (musical and otherwise) by my grandparents, and others who have come before me.</p>`
+  and paths made possible for me (musical and otherwise) by my grandparents, and others who have come before me.`]
 },
 {
   id: 'colters-hell',
@@ -23,7 +44,20 @@ export const musical_works = [{
   imgId: "colters-hell-img",
   has2ndImg: false,
   notesId: 'colters-hell-notes',
-  description: "<p>Dr. Smith approached me about writing a second work for her in late 2018. Again drawing from her evocative oil paintings, I chose her depiction of Colter's Hell (shown left), a series of hot springs and geysers that lie along the mouth of the Shoshone River's canyon, as inspiration for the music. The painting depicts an eruption from one of those geysers.</p> <p>The opening gesture of Colter’s Hell mirrors that moment; it is an eruption, an explosion of sound that starts low to the ground and ascends into a dissipating mist, high up into the air. The music explores these natural associations, translated into musical tones, in two movements - “Eruption” and “Sleeping River”.</p> <p>For flute and piano. Duration 10 minutes.</p> <div class='link-wrapper'> <a href='assets\\scores\\colters-hell-score.pdf' class='description-link' target='_blank'>Score</a> <a href='https://www.youtube.com/watch?v=jw3D_z5qa2s&feature=youtu.be' class='description-link' target='_blank'>Recording</a> </div>"
+  description: [
+    `Dr. Smith approached me about writing a second work for her in late 2018. Again drawing from her evocative oil paintings, I chose her depiction of Colter's Hell
+    (shown left), a series of hot springs and geysers that lie along the mouth of the Shoshone River's canyon, as inspiration for the music. The painting depicts an
+    eruption from one of those geysers.`
+    ,
+    `The opening gesture of Colter’s Hell mirrors that moment; it is an eruption, an explosion of sound that starts low to the ground and ascends into a dissipating
+    mist, high up into the air. The music explores these natural associations, translated into musical tones, in two movements - “Eruption” and “Sleeping River”.`
+    ,
+    `For flute and piano. Duration 10 minutes.`
+  ],
+  links: [
+    {href: 'assets\\scores\\colters-hell.pdf', text: "Score"},
+    {href: 'https://www.youtube.com/watch?v=jw3D_z5qa2s&feature=youtu.be', text: "Recording"}
+  ]
 },
 {
   id: 'r4ch43l',
@@ -33,7 +67,14 @@ export const musical_works = [{
   imgId: 'R4CH43L-img',
   has2ndImg: false,
   notesId: 'r4ch43l-notes',
-  description: "<p>In my film scoring debut, Dr. Robert Kramarczuk, a Holocaust survivor, encounters a demon from his past and 50 years of buried trauma come rushing to the surface.</p> <p>Winner of the Bronze Award at Worldfest Houston REMI Film Festival. Produced by Now What Studios and directed by Noah Bewley.</p> <div class='link-wrapper'> <a href='https://www.imdb.com/title/tt7557660/' target='_blank' class='description-link'>IMDB</a> </div>"
+  description: [
+    `In my film scoring debut, Dr. Robert Kramarczuk, a Holocaust survivor, encounters a demon from his past and 50 years of buried trauma come rushing to the surface.`
+    ,
+    `Winner of the Bronze Award at Worldfest Houston REMI Film Festival. Produced by Now What Studios and directed by Noah Bewley.`
+  ],
+  links: [
+    {href: 'https://www.imdb.com/title/tt7557660/', text: 'IMDB'}
+  ]
 },
 {
   id: 'ekmousike-entry',
@@ -46,7 +87,24 @@ export const musical_works = [{
   img2Alt: 'Oil painting of Trifid Nebula (Dr. Janet Smith)',
   img2Id: 'trifid',
   notesId: 'ekmousike-notes',
-  description: "<p>The word \"ekmousiké\" is personal adaptation the poetic term \"ekphrasis\", which is used to refer to a vivid rhetorical description of a scene or, more commonly, a work of art. The word has Greek roots; \"ek\" is a prefix meaning \"out\" or \"of\", and \"phrasis\", which translates as \"to speak\". In other words, an \"ekphrastic poem\" is a prose approximation of a work of art. I wanted to achieve the same effect but with music, so instead of \"phrasis\", I substituted the Greek word for music, \"mousiké\" (pronounced moo-zee-kay).</p> <p>Dedicated to my grandmother, Dr. Janet Smith, each movement is based off of one of her paintings or poems, and is intended to be a \"vivid musical description\" in the same way an ekphrastic poem would be. In order:</p> <ul id='ekmousike-list'> <li>I. Sunset on Barren River Lake (painting, pictured top left)</li> <li>II. Alone in Mammoth Cave (poem)</li> <li>III. The Color of Death (poem)</li> <li>IV. Winter Peace (painting)</li> <li>V. Trifid Nebula (painting, pictured bottom left)</li> </ul> <p>This work was completed to fulfill my thesis requirement for a Master's Degree in Music Composition. For solo piano. Duration 45 minutes.</p> <div class='link-wrapper'> <a href='assets\\scores\\five-ekmousikes-thesis.pdf' class='description-link' target='_blank'>Score </a> <a href='https://www.youtube.com/playlist?list=PLiDgL0LMN2uOKa0r0rSukHwjiWi7lBe3H' target='_blank' class='description-link'>Recordings</a> </div>"
+  description: [
+    `The word \"ekmousiké\" is personal adaptation the poetic term \"ekphrasis\", which is used to refer to a vivid rhetorical description of a scene or, more
+    commonly, a work of art. The word has Greek roots; \"ek\" is a prefix meaning \"out\" or \"of\", and \"phrasis\", which translates as \"to speak\". In other words,
+    an \"ekphrastic poem\" is a prose approximation of a work of art. I wanted to achieve the same effect but with music, so instead of \"phrasis\", I substituted the
+    Greek word for music, \"mousiké\" (pronounced moo-zee-kay).`,
+    `Dedicated to my grandmother, Dr. Janet Smith, each movement is based off of one of her paintings or poems, and is intended to be a \"vivid musical description\"
+    in the same way an ekphrastic poem would be. In order:`,
+    `I. Sunset on Barren River Lake (painting, pictured top left)`,
+    `II. Alone in Mammoth Cave (poem)`,
+    `III. The Color of Death`,
+    `IV. Winter Peace (painting)`,
+    `V. Trifid Nebula (painting, pictured bottom left)`,
+    `This work was completed to fulfill my thesis requirement for a Master's Degree in Music Composition. For solo piano. Duration 45 minutes.`
+  ],
+  links: [
+    {href: 'assets\\scores\\five-ekmousikes-thesis.pdf', text: 'Score'},
+    {href: 'https://www.youtube.com/playlist?list=PLiDgL0LMN2uOKa0r0rSukHwjiWi7lBe3H', text: 'Recordings'}
+  ]
 },
 {
   id: 'isolation-chamber',
@@ -56,6 +114,15 @@ export const musical_works = [{
   imgId: 'isolation-chamber-img',
   has2ndImg: false,
   notesId: 'isolation-notes',
-  description: "<p>My first album of composed music.</p> <p>In graduate school, I was learning a lot about electronic production and recording techniques while simultaneously experimenting with electrifying my saxophone sound with a guitar pedal and midi controllers in FL Studio. Each work was created entirely by me, blending acoustically and electronically generated sounds, seeking to capture moods and moments from my own life. Thus, the title \"Isolation Chamber\". The works range from meditative ambience to IDM tone poems about broken bill accepting machines.</p> <div class='link-wrapper'> <a href='https://www.youtube.com/playlist?list=PLiDgL0LMN2uP-OPxEmmO0O_RHj5b8-xzk' class='description-link' target='_blank'>Link to Album</a> </div>"
+  description: [
+    `My first album of composed music.`,
+    `In graduate school, I was learning a lot about electronic production and recording techniques while simultaneously experimenting with electrifying my saxophone
+    sound with a guitar pedal and midi controllers in FL Studio. Each work was created entirely by me, blending acoustically and electronically generated sounds,
+    seeking to capture moods and moments from my own life. Thus, the title \"Isolation Chamber\". The works range from meditative ambience to IDM tone poems about
+    broken bill accepting machines.`
+  ],
+  links: [
+    {href: 'https://www.youtube.com/playlist?list=PLiDgL0LMN2uP-OPxEmmO0O_RHj5b8-xzk', text: 'Link to Album'}
+  ]
 }
 ];
